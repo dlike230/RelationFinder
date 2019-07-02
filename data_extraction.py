bb6a156c653ae59f6b696daad365f19cc96ee3cb
dlike230 <dlike230@gmail.com>
2019-07-02 09:44:28 -0400

    Finished document segments

--- data_extraction.py
+++ data_extraction.py
@@ -25,19 +25,44 @@ class DistanceModel:
         target_term_finder = WalkableEntityTree()
         target_term_finder.push(self.target_term)
         total_accumulation = 0
-        for sentence in self.sentences:
+        for s, sentence in enumerate(self.sentences):
             target_term_finder.reset()
             for i, word in enumerate(sentence.words):
                 target_term_list = list(target_term_finder.accept_word(word))
                 if len(target_term_list) == 0:
                     continue
-                target_term = target_term_list[0]
-                yield i, total_accumulation, sentence
+                yield i, total_accumulation, s
                 total_accumulation += 1
 
-    def compute_segments(self):
-        pass
+    def compute_segment_endpoints(self):
+        segment_centers = [segment_center for segment_center in self.compute_segment_centers()]
+        start = 0, self.sentences[0]
+        if len(segment_centers) < 2:
+            end = len(self.sentences[-1].words), self.sentences[-1]
+            yield start, end
+            return
+        for (first_word_index, start_accumulation, start_sentence), (
+                second_word_index, end_accumulation, end_sentence) in zip(segment_centers[:-1], segment_centers[1:]):
+            current_accumulation = 0
+            target_accumulation = (start_accumulation + end_accumulation) // 2
+            for s, sentence in enumerate(self.sentences[start_sentence: end_sentence + 1]):
+                sentence_words = sentence.words
+                accumulation_after_sentence = current_accumulation + len(sentence_words)
+                if accumulation_after_sentence <= target_accumulation:
+                    current_accumulation += accumulation_after_sentence
+                    first_word_index = 0
+                    continue
+                for i, word in enumerate(sentence_words[first_word_index:]):
+                    current_accumulation += 1
+                    if current_accumulation >= target_accumulation:
+                        sentence_index = s + start_sentence
+                        word_index = i + first_word_index
+                        yield sentence_index, word_index
+                first_word_index = 0
 
+    def compute_segments(self):
+        endpoints = [endpoint for endpoint in self.compute_segment_endpoints()]
+        return list(zip(endpoints[:-1], endpoints[1:]))
 
 
 class WikiPage:
